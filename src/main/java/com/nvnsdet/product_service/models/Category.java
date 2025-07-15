7c7a4df1a7c69980906794e052db8dc905dcfc97
Naveenraj D <naveen1211.ssn@gmail.com>
2025-07-15 21:58:55 +0530

    BatchSize with N+1 problem demo

--- src/main/java/com/nvnsdet/product_service/models/Category.java
+++ src/main/java/com/nvnsdet/product_service/models/Category.java
@@ -6,6 +6,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
@@ -21,6 +22,7 @@ public class Category extends BaseModel {
     private String description;
     @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
     @Fetch(FetchMode.SELECT)
+    @BatchSize(size = 2)
     private List<Product> products;
 
 
--- src/test/java/com/nvnsdet/product_service/repos/CategoryRepoTest.java
+++ src/test/java/com/nvnsdet/product_service/repos/CategoryRepoTest.java
@@ -2,9 +2,12 @@ package com.nvnsdet.product_service.repos;
 
 import com.nvnsdet.product_service.models.Category;
 import com.nvnsdet.product_service.models.Product;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class CategoryRepoTest {
@@ -12,6 +15,7 @@ public class CategoryRepoTest {
     CategoryRepo categoryRepo;
 
     @Test
+    @Transactional
     public void testGetCategory(){
         Category category = categoryRepo.findById(13l).get();
         System.out.println("Category Name: " + category.getName());
@@ -19,5 +23,17 @@ public class CategoryRepoTest {
             System.out.println("Product Name: " + product.getName());
         }
 
+    }
+
+    //BatchSize with Select is better than subselect for N+1 Problem
+    @Test
+    @Transactional
+    public void testAllCategoryProducts() {
+        List<Category> categoryList = categoryRepo.findAll();
+        for(Category category : categoryList) {
+            for(Product product : category.getProducts()) {
+                System.out.println(product.getName());
+            }
+        }
     }
 }
