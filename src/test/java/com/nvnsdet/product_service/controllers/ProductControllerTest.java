bbcddb5766c05668049a1d167ca288c7f6122083
Naveenraj D <naveen1211.ssn@gmail.com>
2025-07-18 18:23:39 +0530

    added unit test for an exception scenario

--- src/test/java/com/nvnsdet/product_service/controllers/ProductControllerTest.java
+++ src/test/java/com/nvnsdet/product_service/controllers/ProductControllerTest.java
@@ -1,6 +1,7 @@
 package com.nvnsdet.product_service.controllers;
 
 import com.nvnsdet.product_service.dtos.ProductDto;
+import com.nvnsdet.product_service.exceptions.ProductNotFoundException;
 import com.nvnsdet.product_service.models.Product;
 import com.nvnsdet.product_service.services.IProductService;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 
 
@@ -20,29 +22,43 @@ class ProductControllerTest {
 
     @Mock
     private IProductService productService;
+
     @InjectMocks
     private ProductController productController;
 
     @Test
     public void TestProductDetailsById_WithValidProductId_ShouldReturnProductDetails() {
         // Arrange
-        Long productId = 1L;
-        Product mockProduct = new Product();
-        mockProduct.setId(productId);
-        mockProduct.setName("Test Product");
-
-        when(productService.getProductById(productId)).thenReturn(mockProduct);
+        Long id = 1L;
+        Product product = new Product();
+        product.setId(id);
+        product.setName("Test Product");
+        product.setPrice(100000D);
+        when(productService.getProductById(id)).thenReturn(product);
 
         // Act
-        ResponseEntity<ProductDto> response = productController.getProductById(productId);
+        ResponseEntity<ProductDto> response = productController.getProductById(id);
         ProductDto productDto = response.getBody();
 
         // Assert
         assertNotNull(response);
         assertEquals(200, response.getStatusCode().value());
         assertNotNull(productDto);
-        assertEquals(productId, productDto.getId());
+        assertEquals(id, productDto.getId());
         assertEquals("Test Product", productDto.getName());
+        assertEquals(100000D, productDto.getPrice());
+    }
+
+    @Test
+    public void TestGetProductById_WithInvalidProductId_ShouldReturnNotFoundException() {
+        // Arrange
+        Long id = -2L;
+        when(productService.getProductById(id)).thenThrow(new ProductNotFoundException("Product with ID " + id + " does not exist."));
+        // Act
+        Exception exception = assertThrows(ProductNotFoundException.class, () -> productController.getProductById(id));
+
+        // Assert
+        assertEquals("Product with ID " + id + " does not exist.", exception.getMessage());
     }
 
 }
